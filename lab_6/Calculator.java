9bec0cd66733447b945b60490ac2c28562bcace8
mario-chahni <99657240+mario-chahni@users.noreply.github.com>
2022-03-11 18:08:19 +0200

    Add files via upload

---
+++ lab_6/Calculator.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Calculator {
+    private int num1;
+    private int num2;
+    private String operator;
+
+    // Constructor to initialize the calculator
+    public Calculator() {
+        // Get user input
+        Scanner scanCalc = new Scanner(System.in);
+        /**
+         * Continue code here to ask for the first number, the operator, and the second number, IN THAT ORDER
+         */
+         System.out.print("Input the first number:");   
+         int i=scan.nextInt();
+         System.out.print("Input the operator:");
+         String j=scan.nextLine();
+         System.out.print("Input the second number");
+         int a=scan.nextInt();
+        
+         switch (j) {
+         case '+'
+           add(i, a);
+           break;
+        case '-'
+          subtract(i, a);
+          break;
+         case '*'
+            multiply(i, a);
+           break;
+         case '/'
+          divide(i,a);
+           default:
+           break;
+         // Decide on the operation to perform 
+        /**
+         * Insert code to decide on which function to call, based on the operator the user chose
+         * Hint: Switch statement
+         */
+    }
+
+    public String add(int a, int b) {
+         
+        int s=a+b
+         System.out.print(a+" + "+b+" = " +s);
+        
+    }
+
+    public String subtract(int a, int b) {
+        int s=a-b
+        System.out.print(a+" - "+b+" = "+s);
+    }
+
+    public String multiply(int a, int b) {
+        int s=a*b
+      System.out.print(a+" * "+b+" = "+s);
+    }
+
+    public String divide(int a, int b) {
+      double s=(double)a/b
+      System.out.print(a+" / "+b+" = "+s);
+    }
+}
---
+++ lab_6/Employee.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Employee {
+    // Initializing variables
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Employee() {
+        // Get user input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input the employee’s first name:");
+        String firstName=scan.nextLine();
+        System.out.println("Please input the employee’s last name:");
+        String lastName=scan.nextLine();
+        System.out.println("Please input the employee’s age:");
+        int age=scan.nextInt();
+        System.out.print("Please input the employee’s salary:");
+        double salary=nextDouble();
+ 
+      toString()
+        /**
+         * Continue code here to ask for the first name, last name, age, and salary, IN THAT ORDER
+         */
+
+    }
+
+    public String toString() {
+        System.out.print("Employee information: "+firstname + " " + lastname+ "," + " " + age + "," +" " + salary);
+    }
+}
