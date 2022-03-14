1f8dcfc8396bc17e984dbf9856fc38a641f95f40
mario-chahni <99657240+mario-chahni@users.noreply.github.com>
2022-03-14 20:01:32 +0200

    Update EntryPoint.java

--- lab_6/EntryPoint.java
+++ lab_6/EntryPoint.java
@@ -2,8 +2,25 @@ import java.util.Scanner;
 
 public class EntryPoint {
     public static void main (String[] args) {
-        /**
-         * Enjoy <3
-         */
-    }
+        int service;
+      System.out.println("Which service would you like to use? + \n + [1]: Basic week visualizer + \n + [2]: Advanced week visualizer + \n + [3]: Basic calculator + \n + [4]: Employee repertoire");
+    service=scan.nextInt();
+     switch (service) {
+         case 1: BasicWeek basic=new BasicWeek();
+             basic.printDays();
+             break;
+         case 2: AdvancedWeek advanced=new AdvancedWeek();
+             advanced.printDays();
+             break;
+         case 3: Calculator calc=new Calculator();
+             break;
+         case 4: Employee info=new Employee();
+             break;
+             default :
+             System.out.print("Please make sure you pick a number between 1 and 4");
+             scan.nextInt();
+     }}}
+             
+        
+ }
 }
