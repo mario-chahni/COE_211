39dcb7231d2c7c9e56b1429768f12054c241944b
mario-chahni <99657240+mario-chahni@users.noreply.github.com>
2022-03-18 17:59:49 +0200

    Update Calculator.java

--- lab_6/Calculator.java
+++ lab_6/Calculator.java
@@ -5,13 +5,11 @@ public class Calculator {
     private int num2;
     private String operator;
 
-    // Constructor to initialize the calculator
+    
     public Calculator() {
-        // Get user input
+        
         Scanner scan = new Scanner(System.in);
-        /**
-         * Continue code here to ask for the first number, the operator, and the second number, IN THAT ORDER
-         */
+       
          System.out.print("Input the first number:");   
          int num1=scan.nextInt();
          System.out.print("Input the operator:");
@@ -33,19 +31,11 @@ public class Calculator {
             System.out.print(divide(num1,num2));
            default:
            break;
-         // Decide on the operation to perform 
-        /**
-         * Insert code to decide on which function to call, based on the operator the user chose
-         * Hint: Switch statement
-         */
+        
     }
     }
-    /**
-     *
-     * @param 
-     * @param
-     * @return
-     */
+    
+     
     public String add(int a, int b) {
         int s=a+b;
          String v=(a+" + "+b+" = " +s);
